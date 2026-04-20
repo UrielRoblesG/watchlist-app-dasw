19c63adae7ea223ac4733a40a3b2eb609d2c8c7f
UrielRoblesG <urielrobles735@gmail.com>
2026-04-20 16:23:40 -0600

    feat: define user schema with fields for nombre, email, password, rol, and timestamps

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,2 +1,38 @@
 
+import { Schema, SchemaTypes } from 'mongoose';
+
+
+const userSchema = new Schema({
+    nombre: {
+        type: SchemaTypes.Union,
+
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        match: /.+\@.+\..+/
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    rol: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+
 
