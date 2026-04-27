1b61adf308d3ee1930be547b3c1756583dbbe6d4
UrielRoblesG <urielrobles735@gmail.com>
2026-04-27 17:26:12 -0600

    feat: integrate token validation middleware for watchlist routes

--- src/routes/watchlist.routes.js
+++ src/routes/watchlist.routes.js
@@ -4,15 +4,18 @@
 import { Router } from "express";
 import { mostrarHome } from "../controllers/watchlist.controller.js";
 import service from '../services/watchlist.service.js'
+import { validarToken } from "../middlewares/auth.middleware.js";
 
 const router = Router();
+
+router.use(validarToken);
 
 router.get('/', mostrarHome);
 
 router.get('/:id', async (req, res) => {
-    const userId = 1774545626897;
-    const id = req.params.id;
 
+    const { userId } = req.user;
+    const { id } = req.params;
     const watch = await service.obtenerUno({ id, userId });
     console.log(watch);
 
