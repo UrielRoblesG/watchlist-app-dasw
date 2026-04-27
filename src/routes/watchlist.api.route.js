07a9156f4bf03c84030d1f52cb85587e9611bf1f
UrielRoblesG <urielrobles735@gmail.com>
2026-04-27 17:29:33 -0600

    feat: add role-based access control to watchlist routes

--- src/routes/watchlist.api.route.js
+++ src/routes/watchlist.api.route.js
@@ -1,15 +1,38 @@
 import { Router } from "express";
 import { actualizar, crear, eliminar, obtenerTodos, obtenerUno } from "../controllers/watchlist.controller.js";
 import { validarToken } from "../middlewares/auth.middleware.js";
+import { validarRol } from "../middlewares/validar.rol.middleware.js";
+import rolesUsuarios from "../constants/roles.js";
 
 const router = Router();
 
 router.use(validarToken);
 
-router.get('/', obtenerTodos);
-router.get('/:id', obtenerUno);
-router.post('/', crear);
-router.put('/:id', actualizar);
-router.delete('/:id', eliminar);
+router.get('/',
+    validarRol([
+        rolesUsuarios.ADMINISTRADOR,
+        rolesUsuarios.USUARIO
+    ]),
+    obtenerTodos);
+
+router.get('/:id', validarRol([
+    rolesUsuarios.ADMINISTRADOR,
+    rolesUsuarios.USUARIO
+]), obtenerUno);
+
+router.post('/', validarRol([
+    rolesUsuarios.ADMINISTRADOR,
+    rolesUsuarios.USUARIO
+]), crear);
+
+router.put('/:id', validarRol([
+    rolesUsuarios.ADMINISTRADOR,
+    rolesUsuarios.USUARIO
+]), actualizar);
+
+router.delete('/:id', validarRol([
+    rolesUsuarios.ADMINISTRADOR,
+    rolesUsuarios.USUARIO
+]), eliminar);
 
 export { router };
